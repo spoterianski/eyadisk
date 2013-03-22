430514f9db384d2611b68def235112a752005227
Sergey Poterianski <spoterianski@gmail.com>
2013-03-23 00:42:10 +0400

    Added some code from Bobuk

--- eyadisk.py
+++ eyadisk.py
@@ -1,18 +1,80 @@
+import sys, os
 import httplib
 import base64
 import xml.etree.cElementTree as xml
+import BaseHTTPServer
 from cStringIO import StringIO
 from collections import namedtuple
 
-class EYaAuth(object):
-    """docstring for EYaAuth"""
-    def __init__(self, arg):
-        super(EYaAuth, self).__init__()
-        self.arg = arg
+"""
+the Bobuk code
+"""
+OAYR = "https://oauth.yandex.ru/"
+class EYaRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed = urlparse.parse_qs(urlparse.urlparse(self.path).query)
+        if "code" in parsed:
+            YploadRequestHandler._code = parsed["code"][0]
+        self.wfile.write("HTTP/1.0 200 OK")
+        self.send_header("Date", self.date_time_string())
+        self.send_header("Server", self.version_string())
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write("<html><body>\n")
+        self.wfile.write("<script>" +
+                         "var win = window.open('', '_self');win.close();" +
+                         "</script>\n")
+        self.wfile.write("Your code is %s" % EYaRequestHandler._code)
+        self.wfile.write("</body></html>\n")
+        self.finish()
 
+def getKey(YD_APP_ID, YD_APP_SECRET, keyfile):
+    if os.path.isfile(keyfile):
+        return open(keyfile, 'r').read()
+    import webbrowser
+    webbrowser.open_new(
+        OAYR + 'authorize?response_type=code&client_id=' + YD_APP_ID)
+
+    EYaRequestHandler._code = None
+    httpd = BaseHTTPServer.HTTPServer(('', 8714), EYaRequestHandler)
+    httpd.handle_request()
+
+    if EYaRequestHandler._code:
+        code = EYaRequestHandler._code
+    else:
+        code = raw_input('Input your code: ').strip()
+
+    res = requests.post(OAYR + 'token', data=dict(
+        grant_type='authorization_code',
+        code=code,
+        client_id=YD_APP_ID, client_secret=YD_APP_SECRET
+    ))
+    if res.status_code != 200:
+        raise Exception('Wrong code')
+    key = res.json['access_token']
+    with open(keyfile, 'w') as fl:
+        fl.write(key)
+    return key
+
+class LoginAPI:
+    MP = "https://login.yandex.ru/info?format=json"
+
+    def __init__(self, key):
+        self.key = "OAuth " + key
+
+    def getInfo(self):
+        rq = requests.get(self.MP, headers={
+            'Authorization': self.key,
+        })
+        return rq.json
+
+"""
+my code
+"""
 class EYaDisk(object):
     webdav_url = "webdav.yandex.ru"
     File = namedtuple('File', ['name', 'size', 'mtime', 'ctime'])
+
     headers = {}
     all_headers = {'Accept': '*/*',
         'Authorization': '',
@@ -48,35 +110,41 @@ class EYaDisk(object):
     # list directory on server
     def ls(self, folder):
         self._set_headers('folder_status')
-        data = self.request('PROPFIND', '/%s' % folder)
-        tree = xml.parse(StringIO(data))
-        return [self.elem2file(elem) for elem in tree.findall('{DAV:}response')]
+        resp = self.request('PROPFIND', '/%s' % folder)
+        resp_data = xml.parse(StringIO(resp['data']))
+        return [self.elem2file(elem) for elem in resp_data.findall('{DAV:}response')]
 
     # download file from server
     def download(self, remote_file, local_file):
         self._set_headers('download')
-        data = self.request('GET', '/%s' % remote_file)
+        resp = self.request('GET', '/%s' % remote_file)
         with open(local_file, 'wb') as f:
-            f.write(data)
+            f.write(resp['data'])
 
     # upload file to server
     def upload(self, local_file, remote_file):
         f = open(local_file, 'rb')
         data = f.read()
         self._set_headers('upload', len(data))
-        self.request('PUT', remote_file, data)
+        resp = self.request('PUT', remote_file, data)
         f.close()
 
     # delete file or directory on  server
     def delete(self, file):
         self._set_headers('common')
-        return self.request('DELETE', '/%s' % file)
+        resp = self.request('DELETE', '/%s' % file)
 
+    # publish file
+    def publish(self, path):
+        self._set_headers('common')
+        resp = self.request('POST', '/%s' % path + '?publish')
+        if resp['status'] != 302:
+            raise Exception('Wtf?')
 
     # create directory on  server
     def mkdir(self, folder):
         self._set_headers('common')
-        return self.request('MKCOL', '/%s' % folder)
+        resp = self.request('MKCOL', '/%s' % folder)
 
     # create dirs on server (aka mkdir -p)
     def mkdirs(self, path):
@@ -105,7 +173,6 @@ class EYaDisk(object):
             self.cwd = '/' + stripped_path
         else:
             self.cwd += stripped_path
-
 
 
     def prop(self, elem, name, default=None):
@@ -140,6 +207,7 @@ class EYaDisk(object):
         if data:
             conn.send(data)
         response = conn.getresponse()
+        status = response.status
         data = response.read()
         conn.close()
-        return data;
+        return {'status':status, 'data':data }
