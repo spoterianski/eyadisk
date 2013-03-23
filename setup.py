e6387479e77f42fff4a1b3b93efc6bf1dd19dc05
Sergey Poterianski <spoterianski@gmail.com>
2013-03-23 12:31:12 +0400

    fix bug in setup tools

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='eyadisk',
-    version='0.1',
+    version='0.2',
     description='Simple API for access to Yandex.Disk',
     author='Sergey Poterianski',
     author_email='spoterianski@gmail.com',
     url='http://github.com/spoterianski/eyadisk',
-    py_modules=['eyadisk'],
+    scripts=['eyadisk.py'],
     license='MIT',
 )
